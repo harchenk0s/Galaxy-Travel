d21ad5fb0477b0298e92d272e9deca28008b24d4
HAROS <harchenk0s@yandex.ru>
2021-08-05 19:16:38 +0700

    Start work on ship and touch control

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    [SerializeField] private int _maxSpeed;
+    [SerializeField] private int _handling;
+    [SerializeField] private int _armor;
+
+}
---
+++ Assets/Scripts/TouchControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TouchControl : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    public GameObject ship;
+
+    private void Start()
+    {
+        Debug.LogError("START!");
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Drag");
+        ship.transform.position += new Vector3(eventData.delta.x, 0, 0) * 0.05f;
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End");
+    }
+}
