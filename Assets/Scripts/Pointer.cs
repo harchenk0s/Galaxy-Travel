930e7b5577d2f3578a74fff3bcb6faf5db272c11
HAROS <harchenk0s@yandex.ru>
2021-08-06 23:24:34 +0700

    Add pointerBorders and pointer coordinate sync and finding borders coords

--- Assets/Scripts/Pointer.cs
+++ Assets/Scripts/Pointer.cs
@@ -4,5 +4,44 @@ using UnityEngine;
 
 public class Pointer : MonoBehaviour
 {
+    public GameObject ObjectOfControl;
 
+    private float _leftBorderX = -10000f;
+    private float _rightBorderX = 10000f;
+
+    private void Awake()
+    {
+        PointerBorder[] borders = FindObjectsOfType<PointerBorder>();
+
+        if (borders.Length != 0)
+        {
+            float leftBorder = float.MaxValue;
+            float rightBorder = float.MinValue;
+
+            foreach (PointerBorder border in borders)
+            {
+                float borderX = border.transform.position.x;
+
+                if (borderX < leftBorder)
+                    leftBorder = borderX;
+
+                if (borderX > rightBorder)
+                    rightBorder = borderX;
+            }
+
+            _leftBorderX = leftBorder;
+            _rightBorderX = rightBorder;
+        }
+        
+    }
+
+    //private 
+    //private float x;
+    //private float y;
+
+    //public float X
+    //{
+    //    get { return x; }
+    //    set { }
+    //}
 }
--- Assets/Scripts/PointerBorder.cs
+++ Assets/Scripts/PointerBorder.cs
@@ -6,6 +6,22 @@ public class PointerBorder : MonoBehaviour
 {
     public Color GizmoColor = Color.red;
     public Vector3 GizmoSize = new Vector3(0.01f, 1, 1);
+    public Pointer pointer;
+
+    private void Awake()
+    {
+        pointer = FindObjectOfType<Pointer>();
+    }
+
+    private void Start()
+    {
+        if(pointer != null)
+        {
+            Vector3 pointerPos = pointer.transform.position;
+            transform.position = new Vector3(transform.position.x, pointerPos.y, pointerPos.z);
+        }
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = GizmoColor;
