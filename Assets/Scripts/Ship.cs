8555812116509756cc3a168a8b4464e14923e2f9
HAROS <harchenk0s@yandex.ru>
2021-08-08 16:46:03 +0700

    Add ship movement

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -3,6 +3,32 @@
 public class Ship : MonoBehaviour
 {
     [SerializeField] private int _maxSpeed;
-    [SerializeField] private int _handling;
+    [SerializeField] private float _handling;
     [SerializeField] private int _armor;
+
+    private Pointer _pointer = null;
+    private Transform _pointerTransform = null;
+
+    private void Awake()
+    {
+        _pointer = FindObjectOfType<Pointer>();
+    }
+
+    private void Start()
+    {
+        if (_pointer == null)
+        {
+            enabled = false;
+            throw new UnityException("Pointer not found");
+        }
+        else
+        {
+            _pointerTransform = _pointer.transform;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(Mathf.Lerp(transform.position.x, _pointerTransform.position.x, _handling),0,0);
+    }
 }
