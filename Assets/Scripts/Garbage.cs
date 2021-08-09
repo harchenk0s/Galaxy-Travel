474711a8186c32cee3d48d9e9ced824b3628b595
HAROS <harchenk0s@yandex.ru>
2021-08-09 13:31:45 +0700

    Add class garbage
    It rotate object randomly

---
+++ Assets/Scripts/Garbage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Garbage : MonoBehaviour
+{
+    [SerializeField] private float _minRotationSpeed = -0.8f;
+    [SerializeField] private float _maxRotationSpeed = 0.8f;
+    private float _rotationSpeed;
+    private Vector3 _rotation;
+
+    private void Start()
+    {
+        _rotation = new Vector3(Random.value, Random.value, Random.value).normalized;
+        _rotationSpeed = Random.Range(_minRotationSpeed, _maxRotationSpeed);
+    }
+    private void FixedUpdate()
+    {
+        transform.Rotate(_rotation * _rotationSpeed);
+    }
+}
