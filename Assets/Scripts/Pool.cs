4767ac224b5dc839cb1df61c1308ea0df5cd3644
HAROS <harchenk0s@yandex.ru>
2021-08-21 00:55:28 +0700

    Modify pool

--- Assets/Scripts/Pool.cs
+++ Assets/Scripts/Pool.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Pool : MonoBehaviour
 {
     private List<GameObject> _pool = new List<GameObject>();
+    private List<GameObject> _allPoolObjects = new List<GameObject>();
 
     public void Push(GameObject poolObject)
     {
@@ -14,17 +15,29 @@ public class Pool : MonoBehaviour
 
     public GameObject Pop()
     {
+        GameObject poolObject;
+
         if(_pool.Count > 0)
         {
-            GameObject poolObject = _pool[Random.Range(0, _pool.Count - 1)];
-            _pool.Remove(poolObject);
-            poolObject.SetActive(true);
-            return poolObject;
+            poolObject = _pool[Random.Range(0, _pool.Count - 1)];
         }
         else
         {
-            throw new UnityException("Pool is empty!");
+            poolObject = CreateObject(_allPoolObjects[Random.Range(0, _allPoolObjects.Count - 1)]);
         }
+        _pool.Remove(poolObject);
+        poolObject.SetActive(true);
+        return poolObject;
+    }
+
+    public void Clear()
+    {
+        _pool.Clear();
+        foreach (GameObject poolObject in _allPoolObjects)
+        {
+            Destroy(poolObject);
+        }
+        _allPoolObjects.Clear();
     }
 
     public GameObject CreateObject(GameObject prefab)
@@ -32,6 +45,18 @@ public class Pool : MonoBehaviour
         GameObject poolObject = Instantiate(prefab, transform);
         poolObject.SetActive(false);
         _pool.Add(poolObject);
+        _allPoolObjects.Add(poolObject);
         return poolObject;
+    }
+
+    public void CreateObjects(List<GameObject> prefabs)
+    {
+        foreach (GameObject prefab in prefabs)
+        {
+            GameObject poolObject = Instantiate(prefab, transform);
+            poolObject.SetActive(false);
+            _pool.Add(poolObject);
+            _allPoolObjects.Add(poolObject);
+        }
     }
 }
