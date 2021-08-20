254880a4a0a4b25e00ad2ad69c5929cb13f1f6b0
HAROS <harchenk0s@yandex.ru>
2021-08-21 00:55:54 +0700

    Add possibly change algorithm

--- Assets/Scripts/GarbageGenerator.cs
+++ Assets/Scripts/GarbageGenerator.cs
@@ -1,16 +1,64 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
+[RequireComponent(typeof(Pool))]
 public class GarbageGenerator : MonoBehaviour
 {
+    public GenerationAlgorithm _algorithm;
     private Pool _pool;
-    private Vector2 _minBorders;
-    private Vector2 _maxBorders;
+    private Vector2 _minBorders = Vector2.zero;
+    private Vector2 _maxBorders = Vector2.zero;
+    private IEnumerator _generationCorutine;
+
+    public UnityEvent EndWaveEvent;
+
+    public void GenerateWave(float duration)
+    {
+        _generationCorutine = GeneratingWaveCorutine(duration);
+        StartCoroutine(_generationCorutine);
+    }
+
+    public void ChangeAlgorithm(GenerationAlgorithm newAlgorithm)
+    {
+        if(_generationCorutine != null)
+        {
+            StopCoroutine(_generationCorutine);
+            _algorithm.StopGenerate();
+        }
+
+        Destroy(_algorithm);
+        _algorithm = (GenerationAlgorithm)gameObject.AddComponent(newAlgorithm.GetType());
+        _pool.Clear();
+        _pool.CreateObjects(_algorithm.GetPrefabsList());
+    }
+
+    public void Spawn(Vector2 position)
+    {
+        _pool.Pop().transform.position = new Vector3(position.x, position.y, transform.position.z);
+    }
+
+    public void GetBorders(out Vector2 minBorders, out Vector2 maxBorders)
+    {
+        minBorders = _minBorders;
+        maxBorders = _maxBorders;
+    }
 
     private void Awake()
     {
-        _pool = FindObjectOfType<Pool>();
+        _pool = GetComponent<Pool>();
+        _algorithm = GetComponent<GenerationAlgorithm>();
+
+        if(_algorithm == null)
+        {
+            throw new UnityException("Add GenerationAlgorithm");
+        }
+        else
+        {
+            _pool.CreateObjects(_algorithm.GetPrefabsList());
+        }
     }
 
     private void Start()
@@ -26,5 +74,14 @@ public class GarbageGenerator : MonoBehaviour
         {
             throw new UnityException("Add PointerBorders on scene!");
         }
+    }
+
+    private IEnumerator GeneratingWaveCorutine(float duration)
+    {
+        _algorithm.StartGenerate();
+        yield return new WaitForSeconds(duration);
+        _algorithm.StopGenerate();
+        EndWaveEvent.Invoke();
+        _generationCorutine = null;
     }
 }
