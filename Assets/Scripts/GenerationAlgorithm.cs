c5f2602efae71b7cefa2f2d5b3862b4424f3c2db
HAROS <harchenk0s@yandex.ru>
2021-08-21 00:55:17 +0700

    Add class algorithm

---
+++ Assets/Scripts/GenerationAlgorithm.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GenerationAlgorithm : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> prefabs = new List<GameObject>();
+
+    protected GarbageGenerator _generator;
+    protected Vector2 _minBorders = Vector2.zero;
+    protected Vector2 _maxBorders = Vector2.zero;
+    private IEnumerator _generationCorutine;
+
+    public void StartGenerate()
+    {
+        _generator.GetBorders(out _minBorders, out _maxBorders);
+        _generationCorutine = GenerationCorutine();
+        StartCoroutine(_generationCorutine);
+    }
+
+    public void StopGenerate()
+    {
+        if(_generationCorutine != null)
+            StopCoroutine(_generationCorutine);
+    }
+
+    public List<GameObject> GetPrefabsList()
+    {
+        return prefabs;
+    }
+
+    private void Awake()
+    {
+        _generator = GetComponent<GarbageGenerator>();
+        
+    }
+
+    protected abstract IEnumerator GenerationCorutine();
+}
