bc443dcbc95aafec971f8564419252a5369378cf
HAROS <harchenk0s@yandex.ru>
2021-08-21 00:56:12 +0700

    Add random algorithm

---
+++ Assets/Scripts/algo/RandomAlgorithm.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAlgorithm : GenerationAlgorithm
+{
+    protected override IEnumerator GenerationCorutine()
+    {
+        while (true)
+        {
+            Vector2 position =
+                new Vector2(Random.Range(_minBorders.x, _maxBorders.x), Random.Range(_minBorders.y, _maxBorders.y));
+            _generator.Spawn(position);
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.7f));
+        }
+    }
+}
