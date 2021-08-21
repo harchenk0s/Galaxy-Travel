ccc6054e3552dc326e36a85d386e32a0493d20ce
HAROS <harchenk0s@yandex.ru>
2021-08-21 20:29:51 +0700

    Minor change

---
+++ Assets/Scripts/GeneratorAlgorithms/AlgorithmParameters.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AlgorithmParameters : ScriptableObject
+{
+    [SerializeField] protected List<GameObject> _prefabs = new List<GameObject>();
+
+    public List<GameObject> Prefabs => _prefabs;
+}
---
+++ Assets/Scripts/GeneratorAlgorithms/RandomAlgorithm/RandomAlgParameters.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewRandomParam", menuName = "AlgorithmParameters/Random")]
+public class RandomAlgParameters : AlgorithmParameters
+{
+    public string jojo = "dsdsd";
+}
