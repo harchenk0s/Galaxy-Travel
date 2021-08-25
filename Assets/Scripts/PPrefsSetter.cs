a7cab1c08b9032488a02c2fee2cd7bf259a4e47f
HAROS <harchenk0s@yandex.ru>
2021-08-25 19:29:55 +0700

    Add player prefs setter

---
+++ Assets/Scripts/PPrefsSetter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PPrefsSetter : MonoBehaviour
+{
+    public int counts;
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("CountLaunches"))
+        {
+            int countLaunches = PlayerPrefs.GetInt("CountLaunches") + 1;
+            PlayerPrefs.SetInt("CountLaunches", countLaunches);
+            counts = PlayerPrefs.GetInt("CountLaunches");
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            PlayerPrefs.SetInt("CountLaunches", 1);
+            counts = PlayerPrefs.GetInt("CountLaunches");
+            PlayerPrefs.Save();
+        }
+    }
+}
