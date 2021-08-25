75e5ffcf2984912897310304657ccea841bf1cbe
HAROS <harchenk0s@yandex.ru>
2021-08-25 18:50:24 +0700

    Exclude fps counter from build

--- Assets/Scripts/FpsCounter.cs
+++ Assets/Scripts/FpsCounter.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
 
 public class FpsCounter : MonoBehaviour
 {
@@ -9,3 +10,4 @@ public class FpsCounter : MonoBehaviour
         FPS = (int)(1f / Time.unscaledDeltaTime);
     }
 }
+#endif
--- Assets/Scripts/FpsViewer.cs
+++ Assets/Scripts/FpsViewer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿#if UNITY_EDITOR
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -28,3 +29,4 @@ public class FpsViewer : MonoBehaviour
         }
     }
 }
+#endif
