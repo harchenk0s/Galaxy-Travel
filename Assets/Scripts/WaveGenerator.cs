1c5a7b2bf26d3b9e11ff1fea183342994975c507
HAROS <harchenk0s@yandex.ru>
2021-08-25 18:50:40 +0700

    Start work on wave generator

---
+++ Assets/Scripts/WaveGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveGenerator : MonoBehaviour
+{
+    [SerializeField] private List<GenerationAlgorithm> _algorithms = new List<GenerationAlgorithm>();
+
+    public void StartGenerate()
+    {
+
+    }
+}
