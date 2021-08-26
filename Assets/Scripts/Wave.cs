e8b4d9724e7894eebc054a4114b87a0f0366a7b6
HAROS <harchenk0s@yandex.ru>
2021-08-26 23:04:03 +0700

    Add class wave

---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave
+{
+    private string _algorithm;
+    private string _parameters;
+    private float _duration;
+    private float _secondsLeft;
+
+    public Wave(string algorithm, string parameters, float duration)
+    {
+        _algorithm = algorithm;
+        _parameters = parameters;
+        _duration = duration;
+    }
+}
