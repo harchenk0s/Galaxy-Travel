92ab166abca44280bbcc1d51bacdb1f26486ecea
HAROS <harchenk0s@yandex.ru>
2021-08-26 23:46:19 +0700

    Add Start Wave and End Wave methods

--- Assets/Scripts/Wave.cs
+++ Assets/Scripts/Wave.cs
@@ -1,18 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Wave
 {
-    private string _algorithm;
+    private Type _algorithm;
     private string _parameters;
     private float _duration;
-    private float _secondsLeft;
+    private GarbageGenerator _generator;
 
-    public Wave(string algorithm, string parameters, float duration)
+    public bool IsBusy { get; private set; } = false;
+
+    public Wave(Type algorithm, string parameters, float duration, GarbageGenerator generator)
     {
         _algorithm = algorithm;
         _parameters = parameters;
         _duration = duration;
+        _generator = generator;
+    }
+
+    public void StartWave()
+    {
+        IsBusy = true;
+        PlayerPrefs.SetString(_algorithm.ToString(), _parameters);
+        PlayerPrefs.Save();
+
+        _generator.EndWaveEvent.AddListener(EndWave);
+        _generator.ChangeAlgorithm(_algorithm);
+        _generator.GenerateWave(_duration);
+    }
+
+    private void EndWave()
+    {
+        IsBusy = false;
     }
 }
