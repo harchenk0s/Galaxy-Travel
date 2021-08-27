ca2be5aedf08a53a3975d63b49083cb8a99d989f
HAROS <harchenk0s@yandex.ru>
2021-08-27 13:26:26 +0700

    Add SyncShipSpeed class from Garbage into separate class

--- Assets/Scripts/Garbage.cs
+++ Assets/Scripts/Garbage.cs
@@ -8,26 +8,22 @@ public class Garbage : MonoBehaviour
     [SerializeField] private float _minRotationSpeed = -200f;
     [SerializeField] private float _maxRotationSpeed = 200;
 
-    private float _speed = 100f;
+    
     private float _rotationSpeed;
     private Vector3 _rotationVector;
     private Rigidbody _rigidbody;
-    private Ship _ship;
+    
 
     private void Awake()
     {
-        _ship = FindObjectOfType<Ship>();
         _rigidbody = GetComponent<Rigidbody>();
         _rigidbody.useGravity = false;
         _rigidbody.angularDrag = 0;
         _rigidbody.drag = 0;
-        _speed = _ship.CurrentSpeed;
-        _ship.ChangeSpeedEvent.AddListener(OnChangeSpeed);
     }
 
     private void NewParameters()
     {
-        _rigidbody.velocity = -Vector3.forward * _speed;
         _rotationVector = new Vector3(Random.value, Random.value, Random.value);
         _rotationSpeed = Random.Range(_minRotationSpeed, _maxRotationSpeed);
         _rigidbody.AddTorque(_rotationVector * _rotationSpeed);
@@ -36,21 +32,5 @@ public class Garbage : MonoBehaviour
     private void OnEnable()
     {
         NewParameters();
-    }
-
-    private void OnDisable()
-    {
-        _rigidbody.velocity = Vector3.zero;
-    }
-
-    private void OnChangeSpeed(float speed)
-    {
-        _speed = speed;
-        _rigidbody.velocity = -Vector3.forward * _speed;
-    }
-
-    private void OnDestroy()
-    {
-        _ship.ChangeSpeedEvent.RemoveListener(OnChangeSpeed);
     }
 }
---
+++ Assets/Scripts/SyncShipSpeed.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SyncShipSpeed : MonoBehaviour
+{
+    private float _speed = 100f;
+    private Ship _ship;
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _ship = FindObjectOfType<Ship>();
+        _speed = _ship.CurrentSpeed;
+        _ship.ChangeSpeedEvent.AddListener(OnChangeSpeed);
+    }
+
+    private void OnEnable()
+    {
+        _rigidbody.velocity = -Vector3.forward * _speed;
+    }
+
+    private void OnDisable()
+    {
+        _rigidbody.velocity = Vector3.zero;
+    }
+
+    private void OnChangeSpeed(float speed)
+    {
+        _speed = speed;
+        _rigidbody.velocity = -Vector3.forward * _speed;
+    }
+
+    private void OnDestroy()
+    {
+        _ship.ChangeSpeedEvent.RemoveListener(OnChangeSpeed);
+    }
+}
