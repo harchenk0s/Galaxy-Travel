0e7fb7a3082f9c09876ae250436fad2630b710b9
HAROS <harchenk0s@yandex.ru>
2021-08-27 13:28:55 +0700

    Add class spaceGate to ship speed change

---
+++ Assets/Scripts/SpaceGate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SpaceGate : MonoBehaviour
+{
+    [SerializeField] float _boostPercent = 25f;
+
+    public UnityEvent GateUsedEvent;
+
+    public float BoostPercent => _boostPercent;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.rigidbody.TryGetComponent<Ship>(out _))
+        {
+            GateUsedEvent.Invoke();
+        }
+    }
+}
