f426542e12f46f16da768c40d13bdbfe0e2d8525
HAROS <harchenk0s@yandex.ru>
2021-08-30 15:15:46 +0700

    Add class GameMode for set level before game

---
+++ Assets/Scripts/ClassicMode.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassicMode : GameMode
+{
+    private void Awake()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            _waves.Add(new Wave(typeof(GridRandomAlg), "GridRandomAlgDefault", 10f));
+        }
+    }
+}
---
+++ Assets/Scripts/Generation/GameMode.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMode : MonoBehaviour
+{
+    [SerializeField] protected List<Wave> _waves = new List<Wave>();
+
+    public List<Wave> GetWaves()
+    {
+        return new List<Wave>(_waves);
+    }
+}
