a02f9a03f963699955f09eda7c6ea00a461b9d63
HAROS <harchenk0s@yandex.ru>
2021-09-05 18:22:18 +0700

    Create and set camera animation and transitions

---
+++ Assets/Scripts/Animation/CameraAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class CameraAnimation : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void ChangeView()
+    {
+        bool isGameView = _animator.GetBool("IsGameView");
+        _animator.SetBool("IsGameView", !isGameView);
+    }
+}
