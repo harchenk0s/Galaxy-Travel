526d4c02d41c4217c4f092897e8f06b86bcdbf16
HAROS <harchenk0s@yandex.ru>
2021-09-05 18:23:49 +0700

    Add ShipEvents class
    This class is needed to configure additional events without linking them to the ship instance

---
+++ Assets/Scripts/Objects/ShipEvents.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ShipEvents : MonoBehaviour
+{
+    private Ship _ship = null;
+
+    public ChangeSpeedEvent ChangeSpeedEvent;
+    public ChangeArmorEvent ChangeArmorEvent;
+    public UnityEvent SpeedDownEvent;
+    public UnityEvent SpeedUpEvent;
+    public UnityEvent ShipHitEvent;
+    public UnityEvent ShipDeadEvent;
+
+    public void ChangeShip(GameObject ship)
+    {
+        _ship = ship.GetComponent<Ship>();
+        _ship.ChangeSpeedEvent.AddListener(ChangeSpeedEvent.Invoke);
+        _ship.ChangeArmorEvent.AddListener(ChangeArmorEvent.Invoke);
+        _ship.SpeedDownEvent.AddListener(SpeedDownEvent.Invoke);
+        _ship.SpeedUpEvent.AddListener(SpeedUpEvent.Invoke);
+        _ship.ShipHitEvent.AddListener(ShipHitEvent.Invoke);
+        _ship.ShipDeadEvent.AddListener(ShipDeadEvent.Invoke);
+    }
+}
