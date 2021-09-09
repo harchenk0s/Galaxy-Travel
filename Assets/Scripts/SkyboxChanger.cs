ac2c36d852a6d60c6ad528dd4d86d70bde74e070
HAROS <harchenk0s@yandex.ru>
2021-09-09 16:45:46 +0700

    Add skybox changer

---
+++ Assets/Scripts/SkyboxChanger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxChanger : MonoBehaviour
+{
+    [SerializeField] private List<Material> _skyboxes = new List<Material>();
+
+    public void ChangeToRandom()
+    {
+        RenderSettings.skybox = _skyboxes[Random.Range(0, _skyboxes.Count - 1)];
+    }
+}
