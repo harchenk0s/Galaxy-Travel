1989540580dce90adff0daae8ba87bb2607af4a6
HAROS <harchenk0s@yandex.ru>
2021-09-10 03:14:29 +0700

    Prefabs and folders minor changes

---
+++ Assets/Scripts/Other/CustomEvents.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class GameObjectEvent : UnityEvent<GameObject> { }
+[Serializable]
+public class FloatEvent : UnityEvent<float> { }
+[Serializable]
+public class IntEvent : UnityEvent<int> { }
