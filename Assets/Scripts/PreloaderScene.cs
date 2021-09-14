0f611dcf8b9f25ccd1416d3d8aa975ed71716eca
HAROS <harchenk0s@yandex.ru>
2021-09-14 23:50:15 +0700

    Add loading scene

---
+++ Assets/Scripts/PreloaderScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PreloaderScene : MonoBehaviour
+{
+    [SerializeField] Slider _loadingSlider;
+
+    private void Start()
+    {
+        StartCoroutine(AsyncLoadScene());
+    }
+
+    private IEnumerator AsyncLoadScene()
+    {
+        AsyncOperation loadingOperation = SceneManager.LoadSceneAsync(1);
+        while (!loadingOperation.isDone)
+        {
+            _loadingSlider.value = loadingOperation.progress;
+            yield return null;
+        }
+    }
+}
