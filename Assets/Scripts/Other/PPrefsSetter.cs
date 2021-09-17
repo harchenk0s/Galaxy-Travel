1308823a99586f19cc9f68a91cc10d90da23be3f
HAROS <harchenk0s@yandex.ru>
2021-09-17 18:29:35 +0700

    final fixes

--- Assets/Scripts/Other/PPrefsSetter.cs
+++ Assets/Scripts/Other/PPrefsSetter.cs
@@ -12,11 +12,11 @@ public class PPrefsSetter : MonoBehaviour
 
         if (countLaunches == 1)
         {
-            PlayerPrefs.SetInt(Strings.PlayerPrefs.Ship, 0);
-            PlayerPrefs.SetString(Strings.Algorithms.CenterAlg, "CenterAlgDefault");
+            PlayerPrefs.SetInt(Strings.PlayerPrefs.Wallet, 0);
+            PlayerPrefs.SetString(Strings.Algorithms.CenterAlg, Strings.AlgorithmsParameters.CenterAlg.CenterAlgDefault);
             PlayerPrefs.SetString(Strings.PlayerPrefs.Ship, Strings.Ships.SmallFighter);
             PlayerPrefs.SetString(Strings.PlayerPrefs.ShipDefault, Strings.Ships.SmallFighter);
-            PlayerPrefs.SetString(Strings.PlayerPrefs.Mode, "ClassicMode");
+            PlayerPrefs.SetString(Strings.PlayerPrefs.Mode, Strings.Modes.ClassicMode);
             PlayerPrefs.SetInt(Strings.PlayerPrefs.CountLaunches, countLaunches);
             PlayerPrefs.Save();
         }
