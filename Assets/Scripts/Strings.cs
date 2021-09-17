e2800b52289392e10bc408bb6073b764764fe2e1
HAROS <harchenk0s@yandex.ru>
2021-09-17 16:55:04 +0700

    Add class Strings to avoid strings literals

---
+++ Assets/Scripts/Strings.cs
@@ -0,0 +1,65 @@
+﻿public static class Strings
+{
+    public static class PlayerPrefs
+    {
+        public const string Wallet = "Wallet";
+        public const string Mode = "Mode";
+        public const string Ship = "Ship";
+        public const string ShipDefault = "ShipDefault";
+        public const string CountLaunches = "CountLaunches";
+    }
+
+    public static class Ships
+    {
+        public const string SmallFighter = "SmallFighter";
+        public const string Fatty = "Fatty";
+        public const string AsteroidShip = "AsteroidShip";
+        public const string SpaceWorm = "SpaceWorm";
+        public const string Titan = "Titan";
+    }
+
+    public static class Algorithms
+    {
+        public const string CenterAlg = "CenterAlg";
+        public const string RandomAlg = "RandomAlg";
+        public const string GridRandomAlg  = "GridRandomAlg";
+    }
+
+
+    public static class AlgorithmsParameters
+    {
+        public static class CenterAlg
+        {
+            public const string CenterAlgDefault  = "CenterAlgDefault";
+            public const string CenterAlgOnlyCows  = "CenterAlgOnlyCows";
+        }
+
+        public static class GridRandomAlg
+        {
+            public const string GridRandomAlgDefault  = "GridRandomAlgDefault";
+            public const string GridRandomShips  = "GridRandomShips";
+            public const string GridRandomOnlyAsteroids  = "GridRandomOnlyAsteroids";
+            public const string GridRandomOnlyCows  = "GridRandomOnlyCows";
+        }
+
+        public static class RandomAlg
+        {
+            public const string RandomAlgDefault  = "RandomAlgDefault";
+            public const string RandomAlgOnlyCows  = "RandomAlgOnlyCows";
+        }
+    }
+    
+    public static class Modes
+    {
+        public const string ClassicMode  = "ClassicMode";
+        public const string CowMode  = "CowMode";
+        public const string SurvivalMode  = "SurvivalMode";
+        public const string AsteroidJohnMode  = "AsteroidJohnMode";
+    }
+    
+    public static class Animation
+    {
+        public const string CloseTrigger = "CloseTrigger";
+        public const string IsGameView = "IsGameView";
+    }
+}
